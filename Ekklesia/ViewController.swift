71819be63cdbeb7b6045d03f8616ed4110e883c9
sangjun <sangjlee1103@gmail.com>
2023-03-04 17:13:36 +0900

    init: 프로젝트 생성

---
+++ Ekklesia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ekklesia
+//
+//  Created by 이상준 on 2023/03/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
